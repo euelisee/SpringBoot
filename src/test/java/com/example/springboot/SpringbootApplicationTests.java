37e3047e35c39bcb0c6c21e069920ad0a7c1cfd5
Elise Oliveira <euelisee@gmail.com>
2024-01-24 22:38:50 -0300

    Estudos de Java com SpringBoot

---
+++ src/test/java/com/example/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,18 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.example.springboot;
+
+import org.junit.jupiter.api.Test;
+
+
+class SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello world :)");
+
+    }
+}
